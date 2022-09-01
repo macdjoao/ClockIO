b15982df4c50e94fe20d42abd3683093d9e609e7
João Macedo <macdjoao@gmail.com>
2022-09-01 02:47:49 -0300

    Creating new tables

--- app/models.py
+++ app/models.py
@@ -1,9 +1,56 @@
+from datetime import datetime
 from . import db
 
 # Migrate updates to the database
 # $ flask db stamp head
 # $ flask db migrate
 # $ flask db upgrade
+
+class Administrator(db.Model):
+    __tablename__ = 'administrators'
+
+    administrator_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    administrator_name = db.Column(db.String(255), nullable=False)
+    administrator_password_hash = db.Column(db.String(255), nullable=False)
+    administrator_first_access = db.Column(db.Boolean, default=False)
+
+    def __init__(self, administrator_id, administrator_name, administrator_password_hash, administrator_first_access):
+        self.administrator_id = administrator_id
+        self.administrator_name = administrator_name
+        self.administrator_password_hash = administrator_password_hash
+        self.administrator_first_access = administrator_first_access
+
+    def __repr__(self):
+        return f'<administrator_id: {self.administrator_id}, administrator_name: {self.administrator_name}, administrator_password_hash: {self.administrator_password_hash}, administrator_first_access: {self.administrator_first_access}>'
+
+    def to_json(self):
+        return {"administrator_id": self.administrator_id, "administrator_name": self.administrator_name, "administrator_password_hash": self.administrator_password_hash, "administrator_first_access": self.administrator_first_access}
+
+
+class AdministratorLogs(db.Model):
+    __tablename__ = 'administratorlogs'
+
+    administratorlogs_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    administratorlogs_datetime = db.Column(db.DateTime, default=datetime.utcnow)
+    administratorlogs_type = db.Column(db.String(255), nullable=False)
+    administratorlogs_administrator_id = db.Column(db.Integer, db.ForeignKey("administrators.administrator_id"))
+    administratorlogs_action = db.Column(db.Text, nullable=False)
+
+    employee = db.relationship('Administrator', foreign_keys=administratorlogs_administrator_id)
+
+    def __init__(self, administratorlogs_id, administratorlogs_datetime, administratorlogs_type, administratorlogs_administrator_id, administratorlogs_action):
+        self.administratorlogs_id = administratorlogs_id
+        self.administratorlogs_datetime = administratorlogs_datetime
+        self.administratorlogs_type = administratorlogs_type
+        self.administratorlogs_administrator_id = administratorlogs_administrator_id
+        self.administratorlogs_action = administratorlogs_action
+
+    def __repr__(self):
+        return f'<administratorlogs_id: {self.administratorlogs_id}, administratorlogs_datetime: {self.administratorlogs_datetime}, administratorlogs_type: {self.administratorlogs_type}, administratorlogs_administrator_id: {self.administratorlogs_administrator_id}, administratorlogs_action: {self.administratorlogs_action}>'
+
+    def to_json(self):
+        return {"administratorlogs_id": self.administratorlogs_id, "administratorlogs_datetime": self.administratorlogs_datetime, "administratorlogs_type": self.administratorlogs_type, "administratorlogs_administrator_id": self.administratorlogs_administrator_id, "administratorlogs_action": self.administratorlogs_action}
+
 
 class Employee(db.Model):
     __tablename__ = 'employees'
@@ -28,7 +75,32 @@ class Employee(db.Model):
 
     def to_json(self):
         return {"employee_id": self.employee_id, "employee_cpf": self.employee_cpf, "employee_email": self.employee_email, "employee_name": self.employee_name, "employee_password_hash": self.employee_password_hash, "employee_first_access": self.employee_first_access}
-      
+
+
+class EmployeeLogs(db.Model):
+    __tablename__ = 'employeelogs'
+
+    employeelogs_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    employeelogs_datetime = db.Column(db.DateTime, default=datetime.utcnow)
+    employeelogs_type = db.Column(db.String(255), nullable=False)
+    employeelogs_employee_id = db.Column(db.Integer, db.ForeignKey("employees.employee_id"))
+    employeelogs_action = db.Column(db.Text, nullable=False)
+
+    employee = db.relationship('Employee', foreign_keys=employeelogs_employee_id)
+
+    def __init__(self, employeelogs_id, employeelogs_datetime, employeelogs_type, employeelogs_employee_id, employeelogs_action):
+        self.employeelogs_id = employeelogs_id
+        self.employeelogs_datetime = employeelogs_datetime
+        self.employeelogs_type = employeelogs_type
+        self.employeelogs_employee_id = employeelogs_employee_id
+        self.employeelogs_action = employeelogs_action
+
+    def __repr__(self):
+        return f'<employeelogs_id: {self.employeelogs_id}, employeelogs_datetime: {self.employeelogs_datetime}, employeelogs_type: {self.employeelogs_type}, employeelogs_employee_id: {self.employeelogs_employee_id}, employeelogs_action: {self.employeelogs_action}>'
+
+    def to_json(self):
+        return {"employeelogs_id": self.employeelogs_id, "employeelogs_datetime": self.employeelogs_datetime, "employeelogs_type": self.employeelogs_type, "employeelogs_employee_id": self.employeelogs_employee_id, "employeelogs_action": self.employeelogs_action}
+
 
 class Clock(db.Model):
     __tablename__ = 'clocks'
--- queries.sql
+++ queries.sql
@@ -1,11 +1,20 @@
 -- mysql -u root -p
+-- Password#123
 
 -- MySql DATETIME format: YYYY-MM-DD HH:MM:SS
 
 -- Queries used to do tests
+INSERT INTO administrators (administrator_name, administrator_password_hash) 
+VALUES ('admtest', 'admpassword');
+
+INSERT INTO administratorlogs (administratorlogs_type, administratorlogs_administrator_id, administratorlogs_action) 
+VALUES ('CREATE', 1, 'CREATE A EMPLOYEE <EMPLOYEE_NAME>');
 
 INSERT INTO employees (employee_cpf, employee_email, employee_name, employee_password_hash) 
 VALUES ('12345678900', 'test@mail.com', 'test name', 'password');
+
+INSERT INTO employeelogs (employeelogs_type, employeelogs_employee_id, employeelogs_action) 
+VALUES ('CREATE', 1, 'CREATE A CLOCK');
 
 INSERT INTO clocks (clock_employee_id, clock_input, clock_output) 
 VALUES (1, '2022-08-31 12:00:00', '2022-08-31 18:00:00');
