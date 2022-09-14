65ade92a92352671c07c50eaf7c635e1034961ef
João Macedo <macdjoao@gmail.com>
2022-09-13 21:07:59 -0300

    More forms

--- app/forms.py
+++ app/forms.py
@@ -12,23 +12,30 @@ class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me')
 
+class NewPassword(FlaskForm):
+    # when first_access is true, the user must redefine his password
+    pass
+
 class CreateEmployeeForm(FlaskForm):
     employee_cpf = StringField('employee_cpf', validators=[DataRequired()])
     employee_email = StringField('employee_email', validators=[DataRequired()])
     employee_name = StringField('employee_name', validators=[DataRequired()])
+
+    # must be cpf for default, user must change the password in first access
     employee_password_hash = PasswordField('employee_password_hash', validators=[DataRequired()])
 
 class UpdateEmployeeForm(FlaskForm):
+    # select employee (selectfield)
     employee_cpf = StringField('employee_cpf')
     employee_email = StringField('employee_email')
     employee_name = StringField('employee_name')
+
+    # must change the actual password to employee_cpf
     employee_first_access = BooleanField('employee_first_access')
 
 class DeleteEmployeeForm(FlaskForm):
-    employee_cpf = StringField('employee_cpf', validators=[DataRequired()])
-
-class ReadEmployeeSingleForm(FlaskForm):
-    employee_cpf = StringField('employee_cpf', validators=[DataRequired()])
+    # select employee (selectfield)
+    pass
 
 class CreateClockForm(FlaskForm):
     clock_input = DateTimeField('clock_input', validators=[DataRequired()])
@@ -36,9 +43,22 @@ class CreateClockForm(FlaskForm):
     clock_extra = BooleanField('clock_extra')
 
 class UpdateClockForm(FlaskForm):
+    # select clock (clock_id)
     clock_input = DateTimeField('clock_input')
     clock_output = DateTimeField('clock_output')
     clock_extra = BooleanField('clock_extra')
 
+class ReadClockAll(FlaskForm):
+    # select all registers, with pagination
+    pass
+
 class ReadClockSingleForm(FlaskForm):
-    employee_cpf = StringField('employee_cpf', validators=[DataRequired()])
+    # select employee registers (selectfield)
+    pass
+
+class ReadClockWithFilters(FlaskForm):
+    # select employee (selectfield) (all, one or many)
+    # select first_date
+    # select last_date
+    # select extra
+    pass
